f56d83f79e909c19eff161e78de1696b301c2c98
Rix <rixwoodling@gmail.com>
2026-04-13 21:12:05 -0700

    Add GradientBoostingClassifier and SVC imports

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
 
 # Load dataset from CSV
 def load_data():
